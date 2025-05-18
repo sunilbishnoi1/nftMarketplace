41e8250010da53cc63607addc75e3430f7b03831
Izanagi078 <kashyaparman15@gmail.com>
2025-05-18 22:35:51 +0530

    Backend

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,7 @@
+const Aurora = artifacts.require('Aurora')
+
+module.exports = async (deployer) => {
+  const accounts = await web3.eth.getAccounts()
+
+  await deployer.deploy(Aurora, 'Aurora NFTs', 'ANT', 10, accounts[1])
+}
